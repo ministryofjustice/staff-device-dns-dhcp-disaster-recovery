8331574d549c6767812cf64f06e543b2ab36f527
Touhidul Islam <69799571+Touhidul-Islam-MoJ@users.noreply.github.com>
2021-01-29 11:57:50 +0000

    Added prompt to the script to standardise it for dns and dhcp

--- scripts/restore-service-container.sh
+++ scripts/restore-service-container.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 set -euo pipefail
 
-export AWS_PROFILE=moj-pttp-dev
+export AWS_PROFILE=moj-staff-device-production
 export PAGER=cat
+
+read -p "Which service you want to restore? 'dns'/'dhcp': " service
+if [ !"$service" = "dns" ] || [ !"$service" = "dhcp" ]; then
+  echo "Please enter 'dns' or 'dhcp'.";
+  exit 1;
+fi
 
 aws ecr describe-images \
     --query 'reverse(sort_by(imageDetails,& imagePushedAt))[:5]' \
-    --repository-name staff-device-development-dns-docker --no-paginate --output json | jq '.[] | {imageDigest: .imageDigest, imagePushedAt: .imagePushedAt, imageTags: .imageTags[0]}'
+    --repository-name staff-device-production-$service-docker --no-paginate --output json | jq '.[] | {imageDigest: .imageDigest, imagePushedAt: .imagePushedAt, imageTags: .imageTags[0]}'
 
 read -p "Copy and paste the imageDigest to roll back to: " imageDigest
-MANIFEST=$(aws ecr batch-get-image --repository-name staff-device-development-dns-docker --image-ids imageDigest=$imageDigest --query images[].imageManifest --output text)
-aws ecr put-image --repository-name staff-device-development-dns-docker --image-tag latest --image-manifest "$MANIFEST"
+MANIFEST=$(aws ecr batch-get-image --repository-name staff-device-production-$service-docker --image-ids imageDigest=$imageDigest --query images[].imageManifest --output text)
+aws ecr put-image --repository-name staff-device-production-$service-docker --image-tag latest --image-manifest "$MANIFEST"
 echo "Succesfully rolled back to version: $imageDigest"
 exit
