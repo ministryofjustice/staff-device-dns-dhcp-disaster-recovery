bce402322afffa2e04e1c8481811cafd03997d5f
Touhidul Islam <69799571+Touhidul-Islam-MoJ@users.noreply.github.com>
2021-02-05 15:41:19 +0000

    Rephrased the instruction to read re-tag instead of roll back This warning line was copied from the config restore script Need change as this script re-tags images.

--- scripts/restore-service-container.sh
+++ scripts/restore-service-container.sh
@@ -17,8 +17,8 @@ aws ecr describe-images \
   --query 'reverse(sort_by(imageDetails,& imagePushedAt))[:5]' \
   --repository-name staff-device-"$env"-"$service"-docker --no-paginate --output json | jq '.[] | {imageDigest: .imageDigest, imagePushedAt: .imagePushedAt, imageTags: .imageTags[0]}'
 
-read -p "Copy and paste the imageDigest to roll back to: " imageDigest
+read -p "Copy and paste the imageDigest to re-tag as latest: " imageDigest
 MANIFEST=$(aws ecr batch-get-image --repository-name staff-device-"$env"-"$service"-docker --image-ids imageDigest="$imageDigest" --query images[].imageManifest --output text)
 aws ecr put-image --repository-name staff-device-"$env"-"$service"-docker --image-tag latest --image-manifest "$MANIFEST"
-echo "Succesfully rolled back to version: $imageDigest"
+echo "Successfully re-tagged image: $imageDigest as latest"
 exit
